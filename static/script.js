a6c8cb836cf84e3a0eb18a8cf4436858df52b438
LePetitMarill <d.brefort@hotmail.com>
2016-12-18 22:26:34 +0100

    syntaxennw

--- static/script.js
+++ static/script.js
@@ -14,12 +14,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	var addLine = function(pkmn, txt, datemsg, trclass = null) {
 		var tr = document.createElement('tr');
-        if(trclass)
-            tr.className = trclass;
+		if(trclass) {
+		    tr.className = trclass;
+		}
 
 		var td = document.createElement('td');
-		if(pkmn === 'info')
+		if(pkmn === 'info') {
 			td.appendChild(document.createTextNode('[Info]'));
+		}
 		else
 		{
 			var label = document.createElement('label');
@@ -33,28 +35,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		td = document.createElement('td');
 		td.innerHTML = txt;
-        if(String(txt).match(/^&gt;/))
-            td.className = 'greentext';
+       		if(String(txt).match(/^&gt;/)) {
+            		td.className = 'greentext';
+		}
 		tr.appendChild(td);
 
 		td = document.createElement('td');
 		var dat = (new Date(datemsg)).toLocaleDateString().replace(/ /, '\xa0');
 		var sp = document.createElement('span');
-        if(dt)
+        	if(dt) {
 			sp.className = 'show';
+		}
 		sp.appendChild(document.createTextNode(dat));
 		td.appendChild(sp);
 
 		sp = document.createElement('span');
-        if(dt && hr)
+       		if(dt && hr) {
 			sp.className = 'show';
+		}
 		sp.appendChild(document.createTextNode(',\xa0'));
 		td.appendChild(sp);
 
 		dat = (new Date(datemsg)).toLocaleTimeString().replace(/ /, '\xa0');
 		sp = document.createElement('span');
-        if(hr)
+        	if(hr) {
 			sp.className = 'show';
+		}
 		sp.appendChild(document.createTextNode(dat));
 		td.appendChild(sp);
 		tr.appendChild(td);
@@ -66,32 +72,33 @@ document.addEventListener('DOMContentLoaded', function() {
 	};
 
     var addUser = function(userid, params) {
-        if(userid in users)
+        if(userid in users) {
             return;
+	}
         
         users[userid] = params;
         var mute = muted.indexOf(userid) !== -1;
-        
-		var tr = document.createElement('tr');
-        if(mute)
+	var tr = document.createElement('tr');
+        if(mute) {
             tr.className = 'mute';
+	}
 
-		var td = document.createElement('td');
+	var td = document.createElement('td');
         var label = document.createElement('label');
 
         label.appendChild(document.createTextNode(params.name));
         label.style.color = params.color;
-		label.style.backgroundImage = 'url(".' + params.img + '")';
-		label.className = 'left';
+	label.style.backgroundImage = 'url(".' + params.img + '")';
+	label.className = 'left';
         td.appendChild(label);
-		tr.appendChild(td);
-		td = document.createElement('td');
+	tr.appendChild(td);
+	td = document.createElement('td');
         
         if(!params.you) {
-			var sound = document.createElement('img');
-			sound.src = mute ? './img/mute.png' : './img/speaker.png';
+		var sound = document.createElement('img');
+		sound.src = mute ? './img/mute.png' : './img/speaker.png';
             sound.className = 'sound';
-			td.appendChild(sound);
+		td.appendChild(sound);
 
             sound.onmousedown = function() {
                 mute = muted.indexOf(userid) !== -1;
@@ -108,7 +115,7 @@ document.addEventListener('DOMContentLoaded', function() {
             };
         }
 
-		tr.appendChild(td);
+	tr.appendChild(td);
         usertbl.appendChild(tr);
         users[userid].dom = tr;
     };
@@ -148,36 +155,43 @@ document.addEventListener('DOMContentLoaded', function() {
     rightbtn.onchange = function(evt) {
         left = localStorage.left = !rightbtn.checked;
         var rows = document.querySelectorAll('#chattbl td:first-child label');
-        for(var i = 0; i < rows.length; i++)
-			rows[i].className = left ? 'left' : 'right';
+        for(var i = 0; i < rows.length; i++) {
+		rows[i].className = left ? 'left' : 'right';
+	}
     };
 
     dtbtn.checked = dt;
     dtbtn.onchange = function(evt) {
         localStorage.dt = dt = dtbtn.checked;
         var rows = document.querySelectorAll('#chattbl td:last-child span:first-child');
-        for(var i = 0; i < rows.length; i++)
+        for(var i = 0; i < rows.length; i++) {
 			rows[i].className = dt ? 'show' : '';
-		mid();
+	}
+	mid();
     };
 
     hrbtn.checked = hr;
     hrbtn.onchange = function(evt) {
-        localStorage.hr = hr = hrbtn.checked;
+        localStorage.hr = hrbtn.checked;
+	hr = localStorage.hr;
         var rows = document.querySelectorAll('#chattbl td:last-child span:last-child');
-        for(var i = 0; i < rows.length; i++)
-			rows[i].className = hr ? 'show' : '';
-		mid();
+        for(var i = 0; i < rows.length; i++) {
+		rows[i].className = hr ? 'show' : '';
+	}
+	mid();
     };
 
 	var mid = function() {
-        var atBottom = chatbox.scrollTop === (chatbox.scrollHeight - chatbox.offsetHeight);
-        var rows = document.querySelectorAll('#chattbl td:last-child span:nth-child(2)');
-        for(var i = 0; i < rows.length; i++)
-			rows[i].className = (dt && hr) ? 'show' : '';
-        if(atBottom)
-			for(var i = 150; i > 0; i-= 15)
+		var atBottom = chatbox.scrollTop === (chatbox.scrollHeight - chatbox.offsetHeight);
+		var rows = document.querySelectorAll('#chattbl td:last-child span:nth-child(2)');
+		for(var i = 0; i < rows.length; i++) {
+				rows[i].className = (dt && hr) ? 'show' : '';
+		}
+        	if(atBottom) {
+			for(var i = 160; i > 0; i-= 40) {
 				setTimeout(function() { chatbox.scrollTop = chatbox.scrollHeight; }, i);
+			}
+		}
 	}
 
     ckwipe.onclick = function(evt) {
@@ -272,9 +286,11 @@ document.addEventListener('DOMContentLoaded', function() {
     userswitch.onclick = function() {
         var atBottom = chatbox.scrollTop === (chatbox.scrollHeight - chatbox.offsetHeight);
         userlist.style.width = userlist.style.width == '0px' ? '185px' : '0px';
-        if(atBottom)
-			for(var i = 150; i > 0; i-= 15)
-				setTimeout(function() { chatbox.scrollTop = chatbox.scrollHeight; }, i);
+        if(atBottom) {
+		for(var i = 160; i > 0; i-= 40) {
+			setTimeout(function() { chatbox.scrollTop = chatbox.scrollHeight; }, i);
+		}
+	}
     };
     
     // WebSocket-related functions
@@ -290,17 +306,17 @@ document.addEventListener('DOMContentLoaded', function() {
         
         input.onkeydown = function(evt) {
             if(evt.keyCode == 13 && input.value) {
-                if(input.value.substring(0,7) == "/attack"){
-                    splitted = input.value.split(" ")
+                if(input.value.substring(0,7) == '/attack') {
+                    splitted = input.value.split(' ');
                     ws.send(JSON.stringify({
                         type : 'attack',
                         target : splitted[1],
-                        order : (splitted.length == 3) ? parseInt(splitted[2]) : 0
-                    }))
+                        order : ((splitted.length == 3) ? parseInt(splitted[2]) : 0)
+                    }));
                 }
-                else{
+                else {
                     ws.send(JSON.stringify({type: 'msg', msg: input.value, lang: lang}));
-                    }
+                }
 
                 input.value = '';
             }
@@ -316,55 +332,56 @@ document.addEventListener('DOMContentLoaded', function() {
                 
                 switch(msg.type) {
                     case 'connect':
-						addLine('info', 'Un ' + msg.params.name + ' sauvage apparaît !', (new Date), 'log');
+			addLine('info', 'Un ' + msg.params.name + ' sauvage apparaît !', (new Date), 'log');
                         addUser(msg.userid, msg.params);
                         break;
                     
                     case 'disconnect':
-						addLine('info', 'Le ' + users[msg.userid].name + " sauvage s'enfuit !", (new Date), 'log part');
+			addLine('info', 'Le ' + users[msg.userid].name + " sauvage s'enfuit !", (new Date), 'log part');
                         delUser(msg.userid);
                         break;
 
                     case 'attack':
-
-                        switch(msg['event']){
+                        switch(msg['event']) {
                             case 'attack':
-                                addLine('info', users[msg.attacker_id].name + " attaque " + users[msg.defender_id].name + " !", (new Date), 'log');
+                                addLine('info', users[msg.attacker_id].name + ' attaque ' + users[msg.defender_id].name + ' !', (new Date), 'log');
                                 break;
                             case 'dice':
-                                addLine('info', users[msg.attacker_id].name + " tire un " + msg.attacker_dice + ", "
-                                + users[msg.defender_id].name + " tire un " + msg.defender_dice + "!", (new Date), 'log');
+                                addLine('info', users[msg.attacker_id].name + ' tire un ' + msg.attacker_dice + ', ' + users[msg.defender_id].name + ' tire un ' + msg.defender_dice + ' !', (new Date), 'log');
                                 break;
                             case 'effect':
-                                addLine('info', users[msg.target_id].name + " est maintenant affecté par l'effet " + msg.effect + " !", (new Date), 'log');
+                                addLine('info', users[msg.target_id].name + " est maintenant affecté par l'effet " + msg.effect + ' !', (new Date), 'log');
                                 break;
                             case 'invalid':
                                 addLine('info', "Impossible d'attaquer pour le moment, ou pokémon invalide", (new Date), 'log part');
                                 break;
                             case 'nothing':
-                                addLine('info', "Il ne se passe rien...", (new Date), 'log part');
+                                addLine('info', 'Il ne se passe rien...', (new Date), 'log part');
                                 break;
                             case 'timeout':
-                                addLine('info', "L'effet " + msg.effect + " est terminé.", (new Date), 'log part');
+                                addLine('info', "L'effet " + msg.effect + ' est terminé.', (new Date), 'log part');
                                 break;
                         }
                         break;
 
                     case 'userlist':
-                        for(var i = 0; i < msg.users.length; i++)
+                        for(var i = 0; i < msg.users.length; i++) {
                             addUser(msg.users[i].userid, msg.users[i].params);
+			}
                         break;
                     
                     case 'backlog':
-                        for(var i = 0; i < msg.msgs.length; i++)
-							addLine(msg.msgs[i].user, msg.msgs[i].msg, msg.msgs[i].date, 'backlog');
-						addLine('info', 'Vous êtes connecté', (new Date), 'log');
+                        for(var i = 0; i < msg.msgs.length; i++) {
+				addLine(msg.msgs[i].user, msg.msgs[i].msg, msg.msgs[i].date, 'backlog');
+				addLine('info', 'Vous êtes connecté', (new Date), 'log');
+			}
                         break;
                     
                     case 'msg':
                         lastMuted = muted.indexOf(msg.userid) !== -1;
-                        if(!lastMuted)
-							addLine(users[msg.userid], msg.msg, msg.date);
+                        if(!lastMuted) {
+				addLine(users[msg.userid], msg.msg, msg.date);
+			}
                         break;
                 }
             }
@@ -383,10 +400,11 @@ document.addEventListener('DOMContentLoaded', function() {
         };
         
         ws.onclose = function() {
-            for(var i in users)
+            for(var i in users) {
                 delUser(i);
+	    }
             
-			addLine('info', 'Déconnecté, réessai...', (new Date), 'log part');
+		addLine('info', 'Déconnecté, réessai...', (new Date), 'log part');
             
             window.setTimeout(wsConnect, waitTime);
             waitTime = Math.min(waitTime * 2, 120000);
