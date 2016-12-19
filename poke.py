24b337e93e09ad15425456828b49da5bcc9ae2f4
LePetitMarill <d.brefort@hotmail.com>
2016-12-19 05:12:56 +0100

    Retour des voix espagnoles :^)

--- poke.py
+++ poke.py
@@ -36,7 +36,7 @@ class User:
     """Stores a user's state and parameters, which are also used to render the user's audio messages"""
     lang_voices_mapping = {"fr" : ("fr" , (1, 2, 3, 4, 5, 6, 7)),
                            "en" : ("us" , (1, 2, 3)),
-                           "es" : ("us" , (1, 2)),
+                           "es" : ("es" , (1, 2)),
                            "de" : ("de" , (4, 5, 6, 7))}
 
     volumes_presets = {'fr1': 1.17138, 'fr2': 1.60851,'fr3': 1.01283, 'fr4': 1.0964, 'fr5': 2.64384, 'fr6': 1.35412, 'fr7': 1.96092, 'us1': 1.658, 'us2': 1.7486, 'us3': 3.48104, 'es1': 3.26885, 'es2': 1.84053}
