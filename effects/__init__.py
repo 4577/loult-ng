0f2151e714a4c5749bc5891cefd2e949b3ea711b
hadware <hadwarez@gmail.com>
2017-01-05 01:09:31 +0100

    ptit accent allemand oklm et ptit effet dyslexie tmtc
    Former-commit-id: 2f461359823d0f5c7346fe2d401c929c1492cf82

--- effects/__init__.py
+++ effects/__init__.py
@@ -1,13 +1,15 @@
 from .effects import Effect, SnebwewEffect, ReversedEffect, ReverbManEffect, TouretteEffect, \
     GhostEffect, SpeechMasterEffect, IssouEffect, AmbianceEffect, \
-    PhonemicShuffleEffect, PhonemicNwwoiwwEffect, PhonemicFofoteEffect, AccentMarseillaisEffect
+    PhonemicShuffleEffect, PhonemicNwwoiwwEffect, PhonemicFofoteEffect, AccentMarseillaisEffect, \
+    VocalDyslexia, AccentAllemandEffect
 from .phonems import PhonemList
 
 import random
 AVAILABLE_EFFECTS = [GhostEffect, SnebwewEffect, ReversedEffect, ReverbManEffect, TouretteEffect,
                      SpeechMasterEffect, PhonemicNwwoiwwEffect, PhonemicShuffleEffect,
-                     PhonemicFofoteEffect, AccentMarseillaisEffect, IssouEffect, AmbianceEffect]
-# AVAILABLE_EFFECTS = [] single effects list used when testing
+                     PhonemicFofoteEffect, AccentMarseillaisEffect, IssouEffect, AmbianceEffect,
+                     VocalDyslexia, AccentAllemandEffect]
+# AVAILABLE_EFFECTS = [VocalDyslexia] # single effects list used when testing
 
 
 def get_random_effect() -> Effect:
