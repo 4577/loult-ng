e588c5d4a642feab00fd5d674c9ada85baf521c3
hadware <hadwarez@gmail.com>
2017-01-07 01:54:07 +0100

    Wpse oublièw d'ajouter l'effet dans les dispo
    Former-commit-id: fa076e356dd37880d39827a73c2035eac19aa726

--- effects/__init__.py
+++ effects/__init__.py
@@ -1,14 +1,14 @@
 from .effects import Effect, SnebwewEffect, ReversedEffect, ReverbManEffect, TouretteEffect, \
     GhostEffect, SpeechMasterEffect, IssouEffect, AmbianceEffect, \
     PhonemicShuffleEffect, PhonemicNwwoiwwEffect, PhonemicFofoteEffect, AccentMarseillaisEffect, \
-    VocalDyslexia, AccentAllemandEffect
+    VocalDyslexia, AccentAllemandEffect, CrapweEffect
 from .phonems import PhonemList
 
 import random
 AVAILABLE_EFFECTS = [GhostEffect, SnebwewEffect, ReversedEffect, ReverbManEffect, TouretteEffect,
                      SpeechMasterEffect, PhonemicNwwoiwwEffect, PhonemicShuffleEffect,
                      PhonemicFofoteEffect, AccentMarseillaisEffect, IssouEffect, AmbianceEffect,
-                     VocalDyslexia, AccentAllemandEffect]
+                     VocalDyslexia, AccentAllemandEffect, CrapweEffect]
 # AVAILABLE_EFFECTS = [VocalDyslexia] # single effects list used when testing
 
 
