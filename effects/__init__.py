1a86ac666bb4c6ca32242a0b6b88fac35169a0bb
hadware <hadwarez@gmail.com>
2017-01-08 23:09:41 +0100

    Effects groups functionnal! (and VenerEffect Implemented wallah)

--- effects/__init__.py
+++ effects/__init__.py
@@ -1,7 +1,7 @@
 from .effects import Effect, SnebwewEffect, ReversedEffect, ReverbManEffect, TouretteEffect, \
     GhostEffect, SpeechMasterEffect, IssouEffect, AmbianceEffect, \
     PhonemicShuffleEffect, PhonemicNwwoiwwEffect, PhonemicFofoteEffect, AccentMarseillaisEffect, \
-    VocalDyslexia, AccentAllemandEffect, CrapweEffect, TurboHangoul, MwfeEffect, BeatsEffect
+    VocalDyslexia, AccentAllemandEffect, CrapweEffect, TurboHangoul, MwfeEffect, BeatsEffect, VenerEffect
 from .phonems import PhonemList
 
 import random
@@ -9,7 +9,7 @@ AVAILABLE_EFFECTS = [GhostEffect, SnebwewEffect, ReversedEffect, ReverbManEffect
                      SpeechMasterEffect, PhonemicNwwoiwwEffect, PhonemicShuffleEffect,
                      PhonemicFofoteEffect, AccentMarseillaisEffect, IssouEffect, AmbianceEffect,
                      VocalDyslexia, AccentAllemandEffect, CrapweEffect, TurboHangoul, MwfeEffect]
-# AVAILABLE_EFFECTS = [BeatsEffect, TurboHangoul] # single effects list used when testing
+AVAILABLE_EFFECTS = [BeatsEffect, TurboHangoul, VenerEffect] # single effects list used when testing
 
 
 def get_random_effect() -> Effect:
