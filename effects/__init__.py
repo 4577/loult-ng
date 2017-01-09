70b6a98cf83958f3e1fa79ea7e24fb012e06e8d1
hadware <hadwarez@gmail.com>
2017-01-09 16:02:02 +0100

    création d'un effet du vieux port plus riche, encore à tester
    Former-commit-id: 17bd561ab2c7068e5dc8ae09cf557aafa68971ff

--- effects/__init__.py
+++ effects/__init__.py
@@ -5,11 +5,14 @@ from .effects import Effect, SnebwewEffect, ReversedEffect, ReverbManEffect, Tou
 from .phonems import PhonemList
 
 import random
-AVAILABLE_EFFECTS = [GhostEffect, SnebwewEffect, ReversedEffect, ReverbManEffect, TouretteEffect,
-                     SpeechMasterEffect, PhonemicNwwoiwwEffect, PhonemicShuffleEffect,
-                     PhonemicFofoteEffect, AccentMarseillaisEffect, IssouEffect, AmbianceEffect,
-                     VocalDyslexia, AccentAllemandEffect, CrapweEffect, TurboHangoul, MwfeEffect]
-AVAILABLE_EFFECTS = [TurboHangoul, BeatsEffect, VenerEffect] # single effects list used when testing
+# the multiplier for each effects list sets the "probability" of the effect
+AVAILABLE_EFFECTS = 1 * [IssouEffect, ReversedEffect, VocalDyslexia] + \
+                    2 * [GhostEffect, SnebwewEffect, ReverbManEffect, TouretteEffect, VenerEffect,
+                         SpeechMasterEffect, PhonemicNwwoiwwEffect, PhonemicShuffleEffect,
+                         PhonemicFofoteEffect, AccentMarseillaisEffect, AccentAllemandEffect, MwfeEffect] + \
+                    3 * [AmbianceEffect, CrapweEffect, TurboHangoul] + \
+                    4 * [BeatsEffect]
+AVAILABLE_EFFECTS = [BeatsEffect] # single effects list used when testing
 
 
 def get_random_effect() -> Effect:
