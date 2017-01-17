8cd56fbea4c030bbd2a21368804b3bd836805ba4
hadware <hadwarez@gmail.com>
2017-01-17 15:40:41 +0100

    Le fixennww
    Former-commit-id: a21cf0b76b19813062cb1c0c972fb1cb8b133d72

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,5 @@
 from tools.effects import WpseEffect, SpoinkEffect
+from .unused_effects import ReversedEffect
 from .effects import Effect, SnebwewEffect, ReverbManEffect, TouretteEffect, \
     GhostEffect, SpeechMasterEffect, AmbianceEffect, SitcomEffect, \
     PhonemicNwwoiwwEffect, PhonemicFofoteEffect, AccentMarseillaisEffect, \
@@ -14,7 +15,7 @@ AVAILABLE_EFFECTS = 1 * [] + \
                     2 * [GhostEffect, SnebwewEffect, ReverbManEffect, TouretteEffect, VenerEffect,
                          SpeechMasterEffect, PhonemicNwwoiwwEffect, VocalDyslexia, SitcomEffect,
                          PhonemicFofoteEffect, VieuxPortEffect, AccentAllemandEffect, MwfeEffect,
-                         CrapweEffect, AmbianceEffect, SpoinkEffect] + \
+                         CrapweEffect, AmbianceEffect, SpoinkEffect ] + \
                     3 * [TurboHangoul, BeatsEffect, GodSpeakingEffect] + \
                     4 * [WpseEffect]
 # AVAILABLE_EFFECTS = [MwfeEffect, TurboHangoul, CrapweEffect, SitcomEffect, WpseEffect, SpoinkEffect, GodSpeakingEffect] # single tools list used when testing
