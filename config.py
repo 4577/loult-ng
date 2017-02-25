c986e0dafa890b6bda4e3708ebe40bb7809326a7
hadware <hadwarez@gmail.com>
2017-02-25 14:41:35 +0100

    Tout semble marcher, go merge
    Former-commit-id: 3ec2e32631bc484298efffdaa4ca22a787811b16

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # in seconds, the time a pokemon has to wait before being able to attack again
-ATTACK_RESTING_TIME = 5
+ATTACK_RESTING_TIME = 30
 
 # in seconds, the time during which a users's message are "remembered"
-FLOOD_DETECTION_WINDOW = 2
+FLOOD_DETECTION_WINDOW = 5
 
 # number of messages per second a user has to be sending (on average, during the detection window) to get
 # shadowmuted
-FLOOD_DETECTION_MSG_PER_SEC = 3
+FLOOD_DETECTION_MSG_PER_SEC = 2
 
 # Number of time a punitive message is sent to an attacked shadowmuted user
 PUNITIVE_MSG_COUNT = 20
