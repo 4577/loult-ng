8ba0257ba0e6fcefabd3a1d60c2541e7681f65fc
LePetitMarill <d.brefort@hotmail.com>
2017-02-26 12:18:52 +0100

    pls test b4 commit :<

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ FLOOD_DETECTION_WINDOW = 5
 
 # number of messages per second a user has to be sending (on average, during the detection window) to get
 # shadowmuted
-FLOOD_DETECTION_MSG_PER_SEC = 2
+FLOOD_DETECTION_MSG_PER_SEC = 5
 
 # Number of time a punitive message is sent to an attacked shadowmuted user
 PUNITIVE_MSG_COUNT = 20
 
 # in minutes, the time before a shadowmuted/banned user is able to connect again
-BAN_TIME = 30
+BAN_TIME = 1
