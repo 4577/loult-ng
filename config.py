ed5ffa9442b7260502b6cce0acf216c56e3c4e80
LePetitMarill <d.brefort@hotmail.com>
2017-02-26 22:41:09 +0100

    test
    Former-commit-id: 44c9139854a4e58bdddf0f6802cd24d9f2c5bd97

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 ATTACK_RESTING_TIME = 30
 
 # in seconds, the time during which a users's message are "remembered"
-FLOOD_DETECTION_WINDOW = 10
+FLOOD_DETECTION_WINDOW = 3
 
 # number of messages per second a user has to be sending (on average, during the detection window) to get
 # shadowmuted
-FLOOD_DETECTION_MSG_PER_SEC = 3
+FLOOD_DETECTION_MSG_PER_SEC = 2
 
 # Number of time a punitive message is sent to an attacked shadowmuted user
 PUNITIVE_MSG_COUNT = 50
 
 # in minutes, the time before a shadowmuted/banned user is able to connect again
-BAN_TIME = 1
+BAN_TIME = 10
