a4bec5af232dd39aca30f99e9c49cbdaa86bd85c
4577 <4577>
2017-10-09 19:36:30 +0200

    Fix banning users in pre-handshake state
    Former-commit-id: 6a95ea78bc98173bc41229221a19cedfeb34744f

--- poke.py
+++ poke.py
@@ -285,12 +285,12 @@ class LoultServer:
             return self.send_json(**info)
 
         # before even running the ban, each clients of the concerned user is notified of the ban
-        for client in [client for client in self.channel_obj.clients if client.user.user_id == user_id]:
+        for client in [client for client in self.channel_obj.clients if client.user and client.user.user_id == user_id]:
             client.send_json(type="banned",
                              msg="ofwere")
 
         connected_list = {client.ip for client in self.channel_obj.clients
-                          if client.user.user_id == user_id}
+                          if client.user and client.user.user_id == user_id}
         backlog_list = {ip for userid, ip in self.loult_state.ip_backlog
                         if userid == user_id}
         todo = connected_list | backlog_list
