fbe117fc073a7b528287bf1d54bb19f2136ab4d6
Hadrien Titeux <hadwarez@gmail.com>
2017-10-17 13:55:27 +0200

    Fix sur un effet

--- effects_workbench.py
+++ effects_workbench.py
@@ -15,7 +15,8 @@ from salt import SALT
 from tools.audio_tools import mix_tracks
 from tools import AudioEffect, PhonemicEffect, PoiloEffect, PitchRandomizerEffect, PhonemicFofoteEffect, VowelExchangeEffect
 from tools.effects.effects import SkyblogEffect, AutotuneEffect, GrandSpeechMasterEffect, CrapweEffect, ReverbManEffect, \
-    ContradictorEffect, RobotVoiceEffect, PitchShiftEffect, GodSpeakingEffect, AccentMarseillaisEffect
+    ContradictorEffect, RobotVoiceEffect, PitchShiftEffect, GodSpeakingEffect, AccentMarseillaisEffect, GhostEffect, \
+    AccentAllemandEffect
 from tools.phonems import PhonemList, FrenchPhonems
 from tools.users import User
 
@@ -98,7 +99,7 @@ class SpeechDeformation(PhonemicEffect):
 
 fake_cookie = md5(("6225f3ff26c044c69233193a39466" + SALT).encode('utf8')).digest()
 user = User(fake_cookie, "wesh", None)
-for effect in [PhonemicFofoteEffect()]:
+for effect in [AccentAllemandEffect()]:
     user.state.add_effect(effect)
 
 msg = """Non mais là les mecs faut se détendre si vous voulez sortir moi jme
