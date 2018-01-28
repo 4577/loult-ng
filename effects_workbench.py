669c5875d32a6ad6303a9b5448ef32dcf3202266
Hadrien Titeux <hadwarez@gmail.com>
2018-01-29 00:45:48 +0100

    Terminé d'implem le nouvel effet du tel qui capte mal, et fix du service systemd

--- effects_workbench.py
+++ effects_workbench.py
@@ -5,7 +5,7 @@ import wave
 from asyncio import get_event_loop
 from hashlib import md5
 
-import numpy
+import numpy as np
 import pyaudio
 from pysndfx import AudioEffectsChain
 from scipy.io.wavfile import read
@@ -13,10 +13,12 @@ from scipy.io.wavfile import read
 from salt import SALT
 from tools import AudioEffect, PhonemicEffect
 from tools.audio_tools import mix_tracks
+from tools.effects.effects import BadCellphoneEffect
 from tools.phonems import PhonemList, FrenchPhonems
 from tools.users import User
 
 logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger("pysndfx").setLevel(logging.DEBUG)
 
 
 class AudioFile:
@@ -51,7 +53,7 @@ class TestEffect(AudioEffect):
     NAME = "test"
     TIMEOUT = 30
 
-    def process(self, wave_data: numpy.ndarray):
+    def process(self, wave_data: np.ndarray):
         low_shelf = AudioEffectsChain().bandreject(80, q=10.0)
         high_shelf = AudioEffectsChain().pitch(700)
         return high_shelf(wave_data, sample_in=16000, sample_out=16000)
@@ -61,7 +63,7 @@ class ConvertINT16PCM(AudioEffect):
     NAME = "convert"
     TIMEOUT = 30
 
-    def process(self, wave_data: numpy.ndarray):
+    def process(self, wave_data: np.ndarray):
         return (wave_data * (2. ** 15)).astype("int16")
 
 
@@ -69,45 +71,12 @@ class AddTrackEffect(AudioEffect):
     NAME = "convert"
     TIMEOUT = 30
 
-    def process(self, wave_data: numpy.ndarray):
+    def process(self, wave_data: np.ndarray):
         with open("tools/data/ambiance/war_mood.wav", "rb") as sndfile:
             rate, track_data = read(sndfile)
         # rnd_pos = random.randint(0,len(track_data) - len(wave_data))
         print(len(track_data))
         return mix_tracks(track_data[rate*3:len(wave_data) + rate*5] * 0.4, wave_data, align="center")
-
-
-class BadCellphoneEffect(AudioEffect):
-    NAME = "mauvais réseau"
-    TIMEOUT = 200
-
-    _params_table = {1: (700, "3k", 30, -7),
-                     2: (400, "3.5k", 25, -6),
-                     3: (300, "4k", 22, -6)}
-
-    def __init__(self, signal_strength: int=None):
-        super().__init__()
-        self.signal = signal_strength if signal_strength is not None else random.randint(1,3)
-        self._name = "%i barres de rézo" % self.signal
-        self.hpfreq, self.lpfreq, self.overdrive, self.gain = self._params_table[self.signal]
-
-    @property
-    def name(self):
-        return self._name
-
-
-    def process(self, wave_data: numpy.ndarray):
-        # first, giving the
-        chain = AudioEffectsChain()\
-            .sinc(hpfreq=self.hpfreq, lpfreq=self.lpfreq)\
-            .overdrive(gain=self.gain)\
-            .gain(self.gain)
-        phone_pass = chain(wave_data, sample_in=16000, sample_out=16000)
-        # now we just need to add some interference to the signal if it's 2 or 3
-        if self.signal < 3:
-            pass
-        else:
-            return wave_data
 
 
 class SpeechDeformation(PhonemicEffect):
@@ -126,6 +95,7 @@ class SpeechDeformation(PhonemicEffect):
 fake_cookie = md5(("622545609233193a39466" + SALT).encode('utf8')).digest()
 user = User(fake_cookie, "wesh", None)
 for effect in [BadCellphoneEffect()]:
+    print("Applying effect %s" % effect.name)
     user.state.add_effect(effect)
 
 msg = """Non mais là les mecs faut se détendre si vous voulez sortir moi jme
