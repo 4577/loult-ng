baa42092285e4547d84ce93348bc151ab24e45dd
hadware <hadware@gmail.com>
2018-09-24 13:15:11 +0200

    Ajouté du C4, du rate limit sur le take, et plus d'objets dans l'inventaire commun
    Former-commit-id: 796aea0ea16c7eb8776081e35700469072cad331

--- tools/objects/inventory.py
+++ tools/objects/inventory.py
@@ -20,7 +20,8 @@ class UserInventory:
         self.objects.remove(obj)
 
     def add(self, obj: LoultObject):
-        self.objects.append(obj)
+        if obj not in self.objects:
+            self.objects.append(obj)
 
     def get_object_by_id(self, obj_id: int):
         try:
@@ -30,3 +31,6 @@ class UserInventory:
 
     def search_by_class(self, obj_class):
         return [obj for obj in self.objects if isinstance(obj, obj_class)]
+
+    def remove_by_class(self, obj_class):
+        self.objects = [obj for obj in self.objects if not isinstance(obj, obj_class)]
