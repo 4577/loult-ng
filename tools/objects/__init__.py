b1162bada194be6a88ae9b9c55974619b9e08db7
hadware <hadware@gmail.com>
2018-09-24 17:14:19 +0200

    Ajout des fleuw et fix sur la ceinture
    Former-commit-id: a5ba63c972b4f87223560823c13c9a0f81a22d52

--- tools/objects/__init__.py
+++ tools/objects/__init__.py
@@ -2,12 +2,12 @@ import random
 from .base import LoultObject
 from .objects import (Grenade, SniperRifle, Revolver, RevolverCartridges, SniperBullets, MagicWand,
                       Crown, SimpleInstrument, Scolopamine, WhiskyBottle, PolynectarPotion,
-                      RPG, RPGRocket, Microphone, C4, Detonator, SuicideJacket)
+                      RPG, RPGRocket, Microphone, C4, Detonator, SuicideJacket, Flower)
 
 # objects which can be given to users and are not specifically linked to any events
 AVAILABLE_OBJECTS = [Grenade, SniperBullets, SniperRifle, Revolver, RevolverCartridges, MagicWand,
                      Crown, SimpleInstrument, Scolopamine, WhiskyBottle, PolynectarPotion, RPG,
-                     RPGRocket, Microphone, C4, Detonator, SuicideJacket]
+                     RPGRocket, Microphone, C4, Detonator, SuicideJacket, Flower]
 
 
 def get_random_object() -> LoultObject:
