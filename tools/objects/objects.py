22ede38b797b0078a752efd6e976d07fda1590d2
hadware <hadwarez@gmail.com>
2018-10-02 23:43:22 +0200

    Fix des contributeurs

--- tools/objects/objects.py
+++ tools/objects/objects.py
@@ -231,7 +231,7 @@ class RPGRocket(UsableObject, DestructibleObject):
         users_rpg = [gun for gun in users_rpg if gun.empty]
         if not users_rpg:
             return server.send_json(type="notification",
-                                    msg="Pas de RPG sniper à recharger dans votre inventaire")
+                                    msg="Pas de RPG à recharger dans votre inventaire")
 
         empty_rpg = users_rpg[0]
         empty_rpg.empty = False
@@ -682,7 +682,7 @@ class MilitiaSniper(UsableObject, TargetedObject, MilitiaWeapon):
                          and usr is not target]
         splashed_usrs = ", ".join(usr.poke_params.fullname for usr in splashed_usrs)
         server.channel_obj.broadcast(type="notification",
-                                     msg="%s se sont faits éclabousser par de sang et de cervelle de %s"
+                                     msg="%s se sont faits éclabousser de sang et de cervelle de %s"
                                          % (splashed_usrs, target.poke_params.fullname))
 
 
