8285c25ad4a5f5b7e0f3b3493072934a1716a1bb
hadware <hadwarez@gmail.com>
2019-10-23 00:35:23 +0200

    Ajoutage de la poudre de perlinw pinw pinw
    Former-commit-id: a90e95e562c7e77176cb7d8c4497bb0eb811f0da

--- tools/effects/unused_effects.py
+++ tools/effects/unused_effects.py
@@ -111,44 +111,6 @@ class TurfuEffect(EffectGroup):
         return [hangoul, crapw]
 
 
-class VenerEffect(EffectGroup):
-    TIMEOUT = 120
-    NAME = "YÉ CHAUD"
-
-    class UPPERCASEEffect(ExplicitTextEffect):
-        TIMEOUT = 120
-
-        def process(self, text: str):
-            return text.upper()
-
-    class AmbianceEffect(AudioEffect):
-        """Adds a random mood to the audio"""
-        NAME = "ambiance"
-        TIMEOUT = 120
-        sound_file = path.join(path.dirname(path.realpath(__file__)),
-                                "data/vener/stinkhole_shave_me_extract.wav")
-        gain = 0.8
-
-        def __init__(self):
-            super().__init__()
-            with open(self.sound_file, "rb") as sndfile:
-                self.rate, self.track_data = read(sndfile)
-
-        @property
-        def name(self):
-            return self._name
-
-        def process(self, wave_data: numpy.ndarray):
-            padding_time = self.rate * 2
-            rnd_pos = random.randint(0, len(self.track_data) - len(wave_data) - padding_time)
-            return mix_tracks(self.track_data[rnd_pos:rnd_pos + len(wave_data) + padding_time] * self.gain,
-                              wave_data,
-                              align="center")
-
-    @property
-    def effects(self):
-        return [self.UPPERCASEEffect(), self.AmbianceEffect()]
-
 class AmbianceEffect(AudioEffect):
     """Adds a random mood to the audio"""
     NAME = "ambiance"
