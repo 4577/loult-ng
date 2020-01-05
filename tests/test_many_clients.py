5f55f8560c444dab0725f59aebe2fab7b6016917
= <haspagan@users.noreply.github.com>
2020-01-05 08:44:42 -0600

    websockets client instead of autobahn. cause : #yolo
    Former-commit-id: 08cd964a756bcc90235cc171be126a2c8dc1f236

--- tests/test_many_clients.py
+++ tests/test_many_clients.py
@@ -1,54 +1,38 @@
+#!/usr/bin/env python
+
 import os, sys
 
+# t-thanks Python (or am I a dumb fuck?)
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-from autobahn.asyncio.websocket import WebSocketClientProtocol, \
-    WebSocketClientFactory
-#from datetime import datetime, timedelta
-#from tools.users import UserState
+import asyncio
+import json
+import time
+import websockets
+
+from os import urandom
 from config import MAX_COOKIES_PER_IP
 
-class ManyClientsProtocol(WebSocketClientProtocol):
+async def client(loop):
+    ck = urandom(16).hex()
+    async with websockets.connect(u"ws://127.0.0.1:80/socket/") as websocket:
 
-    def onConnect(self, response):
-        print("Server connected: {0}".format(response.peer))
-
-    def onConnecting(self, transport_details):
-        print("Connecting; transport details: {}".format(transport_details))
-        return None  # ask for defaults
-
-    def onOpen(self):
-        print("WebSocket connection open.")
-
-    def onMessage(self, payload, isBinary):
-        if isBinary:
-            print("Binary message received: {0} bytes".format(len(payload)))
-        else:
-            print("Text message received: {0}".format(payload.decode('utf8')))
-
-    def onClose(self, wasClean, code, reason):
-        print("WebSocket connection closed: {0}".format(reason))
+        while True:
+            data = await websocket.recv()
+            await websocket.recv()
+            if isinstance(data, str):
+                print("[client {}] Message : {}".format(ck, data))
 
 
-if __name__ == '__main__':
-
-    try:
-        import asyncio
-    except ImportError:
-        # Trollius >= 0.3 was renamed
-        import trollius as asyncio
-
-    factory = WebSocketClientFactory(u"wss://127.0.0.1:80/socket/")
-    factory.protocol = ManyClientsProtocol
-
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     tasks = []
 
     for i in range(MAX_COOKIES_PER_IP):
-        task = asyncio.ensure_future(loop.create_connection(factory, '127.0.0.1', 80))
+        task = asyncio.ensure_future(client(loop))
         tasks.append(task)
 
-    # coro = loop.create_connection(factory, '127.0.0.1', 80)
     loop.run_until_complete(asyncio.wait(tasks))
     loop.run_forever()
     loop.close()
+    
