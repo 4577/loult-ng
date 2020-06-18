90daea223c30638ff41d1fbe24303e52d0df5f14
hadware <hadwarez@gmail.com>
2020-06-19 01:19:57 +0200

    Fixes to the objects API, preliminary tests on rendering wav to MP3 before sending
    Former-commit-id: 4efad9d59c72b38dddfd7ca51657f538e385e936

--- effects_workbench.py
+++ effects_workbench.py
@@ -58,7 +58,7 @@ class SpeechDeformation(PhonemicEffect):
 
 fake_cookie = md5(("6225456233193a39466" + SALT).encode('utf8')).digest()
 user = User(fake_cookie, "wesh", None)
-for effect in [ FapEffect()]:
+for effect in []:
     print("Applying effect %s" % effect.name)
     user.state.add_effect(effect)
 
