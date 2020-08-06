eabd98a339b8c783e00443c8f284aaa8a9781c40
hadware <hadwarez@gmail.com>
2020-08-06 03:26:28 +0200

    Reset de l'addresse serveur
    Former-commit-id: 8baba7a0f447ca0fb2644fe3530e11ebc42ee42b

--- static/script.js
+++ static/script.js
@@ -680,8 +680,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	// WebSocket-related functions
 
 	var wsConnect = function() {
-		//ws = new WebSocket(location.origin.replace('http', 'ws') + '/socket' + location.pathname);
-		ws = new WebSocket('wss://loult.family/socket/toast');
+		ws = new WebSocket(location.origin.replace('http', 'ws') + '/socket' + location.pathname);
+		//ws = new WebSocket('wss://loult.family/socket/toast');
 		ws.binaryType = 'arraybuffer';
 
 		var lastMuted = false;
